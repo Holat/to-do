91e36ae6590968257273dae3165679373bf09bad
Holat <105239299+Holat@users.noreply.github.com>
2023-09-01 19:54:46 +0100

    Icon list

---
+++ constants/Icons.ts
@@ -0,0 +1,28 @@
+const Icons = [
+  {
+    name: "note",
+    color: "#1384F8",
+  },
+  {
+    name: "edit",
+    color: "#664BE6",
+  },
+  {
+    name: "message",
+    color: "#FDAD13",
+  },
+  {
+    name: "folder",
+    color: "#1FB473",
+  },
+  {
+    name: "meet",
+    color: "#EC1F78",
+  },
+  {
+    name: "grocery",
+    color: "#F69223",
+  },
+];
+
+export default Icons;
